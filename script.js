424b4a58f8b70c3cee78bb7b9daada45c36df60e
Patricie Vyhlídalová <patina.vy@gmail.com>
2023-12-10 01:48:07 +0100

    Doplnění názvu společnosti + title stránky

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const JustFood = pocetLidi => {
     const cena = pocetLidi * 100
-    document.body.innerHTML += `Catering od Just Food pro ${pocetLidi} lidí za ${cena} Kč.<br>`
+    const nazev = 'Just Food'
+    document.body.innerHTML += `Catering od ${nazev} pro ${pocetLidi} lidí za ${cena} Kč.<br>`
 }
 
 const YourMama = pocetLidi => {
     const cena = pocetLidi * 250
-    document.body.innerHTML += `Catering od Your Mama pro ${pocetLidi} lidí za ${cena} Kč.<br>`
+    const nazev = 'Your Mama'
+    document.body.innerHTML += `Catering od ${nazev} pro ${pocetLidi} lidí za ${cena} Kč.<br>`
 }
 
 const FlavourHaven = pocetLidi => {
     const cena = pocetLidi * 500
-    document.body.innerHTML += `Catering od Flavour Haven pro ${pocetLidi} lidí za ${cena} Kč.<br>`
+    const nazev = 'Flavour Haven'
+    document.body.innerHTML += `Catering od ${nazev} pro ${pocetLidi} lidí za ${cena} Kč.<br>`
 }
 
 JustFood(15)
@@ -32,12 +35,17 @@ const createEvent = (udalost, pocetLidi, spolecnost) => {
     
     if (spolecnost === JustFood) {
         cena = pocetLidi * 100
+        nazev = "Just Food"
     } else if (spolecnost === YourMama) {
         cena = pocetLidi * 250
+        nazev = "Your Mama"
     } else if (spolecnost === FlavourHaven) {
         cena = pocetLidi * 500
+        nazev = "Flavour Haven"
     }
-    document.body.innerHTML += `Událost s názvem ${udalost} s cateringem od ${spolecnost.name} pro ${pocetLidi} ${sklonovaniLidi(pocetLidi)} stojí ${cena} Kč.`
+    document.body.innerHTML += `Událost s názvem ${udalost} s cateringem od ${nazev} pro ${pocetLidi} ${sklonovaniLidi(pocetLidi)} stojí ${cena} Kč.`
 }
 
 createEvent("Vánoce", 4, FlavourHaven)
+
+document.title = "Vyhlídalová - Úkol 7"
